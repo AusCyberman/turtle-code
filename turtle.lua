a2a466f70cc8829017798e34825cd48b0961d060
Will Pierlot <willp@outlook.com.au>
2022-07-08 15:18:54 +1000

    dsadasd

--- turtle.lua
+++ turtle.lua
@@ -74,6 +74,7 @@ setmetatable(Turtle,{ __index = function (key)
 end})
 
 function Turtle.dig(dir)
+   dig = dig or ""
    if turtle["detect"..dir]() then
         Turtle.checkInventory()
         if not turtle["dig"..dir]()  then
