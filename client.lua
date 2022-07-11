15677841633f6e23b34b8808b597466f844d800a
Will Pierlot <willp@outlook.com.au>
2022-07-11 17:49:17 +1000

    make better ig

--- client.lua
+++ client.lua
@@ -7,12 +7,18 @@ local stop = false
 
 
 
-    local savedPrint = _G.print(...)
+local savedPrintfunction = _G.print
 while not stop do 
     client.send("Hello World!")
     local receiv, b = client.receive(1)
         if receiv then
-        _G.print = function(...) client.send(table.concat(table.pack(...), " ")) end
+        _G.print = function(...) return client.send(table.concat(table.pack(...), " ")) end
+        _G.read = function(...) 
+            local res = client.receive(1)
+            if not res then
+                error("no data")
+            end 
+        end
         local res,err = pcall(function() loadstring(receiv)() end)
         if not res then
            print("Err:")
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 #[macro_use]
 extern crate lazy_static;
-use futures::{sink::SinkExt, stream::StreamExt, select};
+use anyhow::Result;
+use futures::future;
+use futures::{select, sink::SinkExt, stream::FusedStream, stream::StreamExt};
 use hyper::{Body, Request, Response};
-use hyper_tungstenite::{tungstenite, HyperWebsocket};
-use std::convert::Infallible;
+use hyper_tungstenite::{tungstenite, HyperWebsocket, WebSocketStream};
+use log::{info, warn};
+use std::io::{stdout, Write};
+use std::{convert::Infallible, io::Stdin};
+use tokio::io::{stdin, AsyncBufReadExt, AsyncRead, AsyncReadExt, BufReader};
+use tokio_stream::wrappers::LinesStream;
 use tungstenite::Message;
-use anyhow::*;
-use log::{info,warn};
 
 lazy_static! {
-        static ref CLIENT_DATA : Vec<u8> = std::fs::read("client.lua").unwrap();
+    static ref CLIENT_DATA: Vec<u8> = std::fs::read("./client.lua").unwrap();
 }
 
 /// Handle a HTTP or WebSocket request.
 async fn handle_request(mut request: Request<Body>) -> Result<Response<Body>> {
-
     // Check if the request is a websocket upgrade request.
     if hyper_tungstenite::is_upgrade_request(&request) {
         let (response, websocket) = hyper_tungstenite::upgrade(&mut request, None)?;
@@ -37,55 +40,59 @@ async fn handle_request(mut request: Request<Body>) -> Result<Response<Body>> {
 /// Handle a websocket connection.
 async fn serve_websocket(websocket: HyperWebsocket) -> Result<()> {
     let mut websocket = websocket.await?;
+    let mut lines_from_stdin = LinesStream::new(BufReader::new(stdin()).lines()).fuse();
+    print!("lua> ");
+    stdout().flush()?;
     loop {
-    select! {
-        message_m = websocket.next() => {
-        match message_m {
-            Some(message) =>
-        match message? {
-            Message::Text(msg) => {
-                info!("[CLIENT]: {}", msg);
-            },
-            Message::Binary(msg) => {
-                println!("Received binary message: {:02X?}", msg);
-                websocket.send(Message::binary(b"Thank you, come again.".to_vec())).await?;
-            },
-            Message::Ping(msg) => {
-                // No need to send a reply: tungstenite takes care of this for you.
-                println!("Received ping message: {:02X?}", msg);
-            },
-            Message::Pong(msg) => {
-                println!("Received pong message: {:02X?}", msg);
+        select! {
+            data = lines_from_stdin.select_next_some() => {
+                let data = data?;
+                websocket.send(Message::Text(data)).await?;
+                print!("lua> ");
+                stdout().flush()?;
             }
-            Message::Close(msg) => {
-                // No need to send a reply: tungstenite takes care of this for you.
-                if let Some(msg) = &msg {
-                    println!("Received close message with code {} and message: {}", msg.code, msg.reason);
-                } else {
-                    println!("Received close message");
+            msg =  websocket.select_next_some() => {
+                match msg? {
+                    Message::Text(text) => {
+                        println!("[CLIENT] {}", text);
+                    },
+                    Message::Binary(bin) => {
+                        info!("Received binary message: {:?}", bin);
+                    },
+                    Message::Ping(bin) => {
+                        info!("Received ping message: {:?}", bin);
+                        websocket.send(Message::Pong(bin)).await?;
+                    },
+                    Message::Pong(bin) => {
+                        info!("Received pong message: {:?}", bin);
+                    },
+                    Message::Close(close) => {
+                        info!("Received close message: {:?}", close);
+                        return Ok(());
+                    },
+                    Message::Frame(_) => {
+                        info!("Received frame message");
+                    }
                 }
-            },
-            Message::Frame(msg) => {
-               unreachable!();
             }
-        },
-        None => {
-            break;
-        }
+            complete => {
+                println!("closed server");
+                break;
+            }
         }
     }
-
-
-}}
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::init();
     let addr: std::net::SocketAddr = "[::1]:3000".parse()?;
     println!("Listening on http://{}", addr);
-    hyper::Server::bind(&addr).serve(hyper::service::make_service_fn(|_connection| async {
-        core::result::Result::Ok::<_, Infallible>(hyper::service::service_fn(handle_request))
-    })).await?;
+    hyper::Server::bind(&addr)
+        .serve(hyper::service::make_service_fn(|_connection| async {
+            core::result::Result::Ok::<_, Infallible>(hyper::service::service_fn(handle_request))
+        }))
+        .await?;
     Ok(())
 }
